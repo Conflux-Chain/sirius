bf099cb1620f351b12fa47150d6beebaf4fecc09
yqrashawn <namy.19@gmail.com>
2020-09-03 16:01:06 +0800

    build(biolerplate): use react-boilerplate, semantic-release, commitizen

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Sirius
+
+---
+
+**📚Documentation:** [Gitbook](https://cansahin.gitbook.io/react-boilerplate-cra-template/)
+
+**🚑 Collection of `'HOW TO'`s:** [Examples & Tutorials for common patterns](https://github.com/react-boilerplate/cra-template-examples)
+
+---
+
+## Start
+
+```shell
+yarn start
+```
---
+++ src/store/reducers.ts
@@ -0,0 +1,27 @@
+/**
+ * Combine all reducers in this file and export the combined reducers.
+ */
+
+import { combineReducers } from '@reduxjs/toolkit';
+
+import { InjectedReducersType } from 'utils/types/injector-typings';
+
+import type { Reducer } from 'redux';
+
+const identity: Reducer<any> = (state: any) => {
+  return state;
+};
+
+/**
+ * Merges the main reducer with the router state and dynamically injected reducers
+ */
+export function createReducer(injectedReducers: InjectedReducersType = {}) {
+  // Initially we don't have any injectedReducers, so returning identity function to avoid the error
+  if (Object.keys(injectedReducers).length === 0) {
+    return identity;
+  } else {
+    return combineReducers({
+      ...injectedReducers,
+    });
+  }
+}
