ab879b14d7dded0c566c63674877a51d14a957b3
tangxuezhi <tangxuezhi@conflux-chain.org>
2020-09-15 15:59:08 +0800

    feat(TablePanel): fixed tip style

--- src/app/components/TablePanel/PanelTip.tsx
+++ src/app/components/TablePanel/PanelTip.tsx
@@ -3,6 +3,26 @@ import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { translations } from '../../../locales/i18n';
 import { PanelContext } from './Panel';
+import styled from 'styled-components';
+import numeral from 'numeral';
+
+const StyledContainer = styled.div`
+  font-size: 14px;
+  font-family: CircularStd-Book, CircularStd;
+  font-weight: normal;
+  color: #74798c;
+  line-height: 18px;
+  margin-top: 32px;
+  margin-bottom: 24px;
+`;
+const StyledSpan = styled.span`
+  font-size: 14px;
+  font-family: CircularStd-Bold, CircularStd;
+  font-weight: bold;
+  color: #0054fe;
+  line-height: 24px;
+  padding: 0 5px;
+`;
 
 function PanelTip({ tipsShow }) {
   const data = useContext(PanelContext);
@@ -11,12 +31,13 @@ function PanelTip({ tipsShow }) {
   if (!tipsShow) return null;
 
   return (
-    <div>
-      {t(translations.blocksAndTransactions.totalBefore)} {data.total}{' '}
+    <StyledContainer>
+      {t(translations.blocksAndTransactions.totalBefore)}
+      <StyledSpan>{numeral(data.total).format('0,0')}</StyledSpan>
       {t(translations.blocksAndTransactions.totalAfter, {
         type: data.type,
       })}
-    </div>
+    </StyledContainer>
   );
 }
 
