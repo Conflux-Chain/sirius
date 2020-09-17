f8cfdb4731685f9411fe5ead57a43d7f157b327e
Angelia <angelia@Angelia-MacBook-Pro.local>
2020-09-17 16:12:27 +0800

    fix: ts compile type error

--- src/app/components/Text/index.tsx
+++ src/app/components/Text/index.tsx
@@ -15,8 +15,7 @@ type MyTextProps = {
 type NativeAttrs = Omit<React.HTMLAttributes<any>, keyof MyTextProps>;
 export declare type Props = MyTextProps & NativeAttrs;
 
-// const Wrapper = styled(Text)<MyTextProps>` // error with Text component
-const Wrapper = styled.span<MyTextProps>`
+const Wrapper = styled(Text)<{ maxWidth: string }>`
   text-overflow: ellipsis;
   overflow: hidden;
   white-space: nowrap;
@@ -24,7 +23,7 @@ const Wrapper = styled.span<MyTextProps>`
   display: ${props =>
     props.maxWidth === undefined ? 'inherit' : 'inline-block'};
   cursor: pointer;
-`;
+` as typeof Text & React.FC<Props & { maxWidth?: string }>;
 
 // note:
 // 1. maxWidth priority is higher than maxCount
@@ -64,65 +63,3 @@ TextComponent.propTypes = {
 };
 
 export default TextComponent;
-
-// ---- working fine version ---
-
-// type MyTextProps = {
-//   children?: React.ReactNode;
-//   maxWidth?: string;
-//   maxCount?: number;
-//   hoverValue?: React.ReactNode;
-//   tooltipConfig?: Partial<TooltipProps>;
-// };
-// type NativeAttrs = Omit<React.HTMLAttributes<any>, keyof MyTextProps>;
-// export declare type Props = MyTextProps & NativeAttrs;
-
-// const Wrapper = styled.span<Props>`
-//   color: red;
-//   text-overflow: ellipsis;
-//   overflow: hidden;
-//   white-space: nowrap;
-//   max-width: ${props => props.maxWidth};
-//   display: ${props =>
-//     props.maxWidth === undefined ? 'inherit' : 'inline-block'};
-//   cursor: pointer;
-// `;
-
-// // note:
-// // 1. maxWidth priority is higher than maxCount
-// // 2. maxCount obly apply to string
-// // 3. if hoverValue if provide, use hoverValue as Tooltip text, other wise use children
-// const TextComponent = ({
-//   children,
-//   maxWidth,
-//   maxCount,
-//   hoverValue,
-//   tooltipConfig,
-//   ...props
-// }: Props) => {
-//   let child: any = children || '';
-//   if (maxWidth === undefined && maxCount && typeof children === 'string') {
-//     child = child.substr(0, maxCount) + '...';
-//   }
-//   const tooltipValue = hoverValue || children;
-//   return (
-//     <Tooltip {...tooltipConfig} text={tooltipValue}>
-//       <Wrapper maxWidth={maxWidth} {...props}>
-//         {child}
-//       </Wrapper>
-//     </Tooltip>
-//   );
-// };
-
-// TextComponent.defaultProps = {
-//   maxCount: 8,
-// };
-
-// TextComponent.propTypes = {
-//   maxCount: PropTypes.number,
-//   maxWidth: PropTypes.string,
-//   hoverValue: PropTypes.node,
-//   tooltipConfig: PropTypes.object,
-// };
-
-// export default TextComponent;
