fd65d27cd05fe3b6586e37a7833cb4d4b753d641
Angelia <angelia@Angelia-MacBook-Pro.local>
2020-10-13 17:38:17 +0800

    fix: test snapshots issue

--- src/app/containers/PackingPage/__tests__/index.test.tsx
+++ src/app/containers/PackingPage/__tests__/index.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { HelmetProvider } from 'react-helmet-async';
+
 import { PackingPage } from '..';
 
-HelmetProvider.canUseDOM = false;
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+  useParams: () => ({
+    txHash: '0x87455a2f9bcaf8421927575d98852c8e43ff6fe8',
+  }),
+}));
 
-const renderComponent = () =>
-  render(
-    <HelmetProvider context={{}}>
-      <PackingPage />
-    </HelmetProvider>,
-  );
+const renderComponent = () => render(<PackingPage />);
 
-describe('<NotFoundPage />', () => {
+describe('<PackingPage />', () => {
   it('should match the snapshot', () => {
     const component = renderComponent();
     expect(component.container.firstChild).toMatchSnapshot();
