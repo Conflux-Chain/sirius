d5814bbec7ab828af51c248c7917b172ada740f1
guichi <guichi.zhao@zerologix.com>
2020-10-14 18:43:07 +0800

    feat：all chart page

---
+++ src/app/components/Chart/Chart.jsx
@@ -0,0 +1,217 @@
+import React, { useState, useRef, useEffect } from 'react';
+import useSWR from 'swr';
+import createDraw from './draw';
+import { appendApiPrefix } from 'utils/api';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+const RECT_HEIGHT = 40;
+const RECT_WIDTH = 90;
+
+const PIXEL_RATIO = (function () {
+  var ctx = document.createElement('canvas').getContext('2d'),
+    dpr = window.devicePixelRatio || 1,
+    bsr =
+      ctx.webkitBackingStorePixelRatio ||
+      ctx.mozBackingStorePixelRatio ||
+      ctx.msBackingStorePixelRatio ||
+      ctx.oBackingStorePixelRatio ||
+      ctx.backingStorePixelRatio ||
+      1;
+
+  return dpr / bsr;
+})();
+
+const DURATIONS = [
+  ['hour', '1H'],
+  ['day', '1D'],
+  ['month', '1M'],
+  ['all', 'ALL'],
+];
+export default function Chart({ width = 500, indicator = 'blockTime' }) {
+  const { plot, isLoading, isError, setDuration, duration } = usePlot('day');
+  const { t } = useTranslation();
+  const small = width < 500;
+  if (isError) {
+    return <div>Error</div>;
+  } else {
+    return (
+      <Container style={{ width }} small={small}>
+        <Title>{t(`${indicator}.title`)}</Title>
+        <Description>{t(`${indicator}.desciption`)}</Description>
+        {true && (
+          <Draw
+            plot={plot}
+            width={width * 0.83}
+            indicator={indicator}
+            small={small}
+          >
+            <Buttons>
+              {DURATIONS.map(([d, key]) => (
+                <Button
+                  key={key}
+                  small={small}
+                  active={d === duration}
+                  onClick={() => setDuration(d)}
+                >
+                  {key}
+                </Button>
+              ))}
+            </Buttons>
+          </Draw>
+        )}
+      </Container>
+    );
+  }
+}
+
+function Draw({
+  plot,
+  indicator,
+  width = 359,
+  height = width * 0.55,
+  small,
+  children,
+}) {
+  const scale = width / 359;
+  const containerRef = useRef(null);
+  const backgroundCanvasRef = useRef(null);
+  const lineCanvasRef = useRef(null);
+
+  let TRI_HEIGHT = 5 * scale;
+  let POPUP_PADDING = 12 * scale;
+
+  let X_AXIS_HEIGHT = 38 * scale;
+  let Y_AXIS_WIDTH = 45 * scale;
+
+  useEffect(() => {
+    let cursorX;
+    const ctxBg = backgroundCanvasRef.current.getContext('2d');
+    ctxBg.setTransform(PIXEL_RATIO, 0, 0, PIXEL_RATIO, 0, 0);
+
+    const ctxLine = lineCanvasRef.current.getContext('2d');
+    ctxLine.setTransform(PIXEL_RATIO, 0, 0, PIXEL_RATIO, 0, 0);
+
+    const { xScale1, draw } = createDraw({
+      TRI_HEIGHT,
+      POPUP_PADDING,
+      RECT_WIDTH,
+      RECT_HEIGHT,
+      Y_AXIS_WIDTH,
+      X_AXIS_HEIGHT,
+      width,
+      height,
+      ctxBg,
+      ctxLine,
+      plot,
+      indicator,
+    });
+    draw();
+    const listener = event => {
+      const { offsetX } = event;
+      cursorX = Math.round(xScale1.invert(offsetX));
+      draw({
+        cursorX,
+      });
+    };
+    const container = containerRef.current;
+    container.addEventListener('mousemove', listener);
+    return () => container.removeEventListener('mousemove', listener);
+  }, [height, indicator, plot, width]);
+
+  return (
+    <CanvasContainer small={small} ref={containerRef} style={{ width, height }}>
+      <canvas
+        ref={backgroundCanvasRef}
+        style={{ width, height }}
+        width={width * PIXEL_RATIO}
+        height={height * PIXEL_RATIO}
+      />
+      <canvas
+        ref={lineCanvasRef}
+        style={{ width, height }}
+        width={width * PIXEL_RATIO}
+        height={height * PIXEL_RATIO}
+      />
+      {children}
+    </CanvasContainer>
+  );
+}
+
+function usePlot(defaultDuration = 'day') {
+  const [duration, setDuration] = useState(defaultDuration);
+  const { data, error } = useSWR(`/dashboard/plot?duration=${duration}`, url =>
+    fetch(appendApiPrefix(url)).then(response => response.json()),
+  );
+  let listData;
+  if (data) {
+    const {
+      result: { list },
+    } = data;
+    listData = list;
+  }
+  return {
+    plot: listData,
+    isLoading: !error && !data,
+    isError: error,
+    setDuration,
+    duration,
+  };
+}
+
+const Container = styled.div`
+  display: inline-block;
+  position: relative;
+  padding: ${props => (props.small ? '8px' : '24px')};
+  box-shadow: 0.8571rem 0.5714rem 1.7143rem -0.8571rem rgba(20, 27, 50, 0.12);
+  border-radius: 5px;
+`;
+
+const Title = styled.div`
+  font-weight: bolder;
+  font-size: 1.1429rem;
+  color: black;
+  line-height: 2.1429rem;
+`;
+
+const Description = styled.div`
+  font-weight: bold;
+  line-height: 1.2143rem;
+  font-size: 0.8571rem;
+  color: #4a6078;
+`;
+
+const Buttons = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  right: -4rem;
+  top: 1rem;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  width: ${props => (props.small ? '27px' : '32px')};
+  height: ${props => (props.small ? '16px' : '20px')};
+  border: 1px solid rgba(59, 85, 145, 0.2);
+  color: #3b5591;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.7143rem;
+  margin-bottom: 0.5714rem;
+  background: ${props => (props.active ? 'rgba(59, 85, 145, 0.2)' : '#ffffff')};
+  box-shadow: 0.8571rem 0.5714rem 1.7143rem -0.8571rem rgba(6, 6, 8, 0.12);
+  border-radius: 0.2857rem;
+`;
+
+const CanvasContainer = styled.div`
+  position: relative;
+  margin-left: ${props => (props.small ? 0 : '1.7143rem')};
+  margin-top: 0.7857rem;
+  canvas {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+`;
---
+++ src/app/components/Chart/draw.js
@@ -0,0 +1,259 @@
+import dayjs from 'dayjs';
+import { scaleLinear } from 'd3-scale';
+import { extent } from 'd3-array';
+
+const NUM_X_GRID = 7;
+const NUM_Y_GRID = 5;
+
+const COLORS = {
+  '1': {
+    line: '#FA5D8E',
+    inner: '#ECA169',
+    outer: '#FBDADA',
+  },
+  '-1': {
+    line: '#1E54FF',
+    inner: '#A6E999',
+    outer: '#E0FBDA',
+  },
+};
+
+const X_AXIS_HEIGHT = 40;
+export default function createDraw({
+  TRI_HEIGHT,
+  POPUP_PADDING,
+  RECT_WIDTH,
+  RECT_HEIGHT,
+  Y_AXIS_WIDTH,
+  // X_AXIS_HEIGHT,
+  width,
+  height,
+  ctxBg,
+  ctxLine,
+  plot,
+  indicator,
+  isSolid,
+}) {
+  const xData = [],
+    yData = [],
+    data = [];
+  range(0, plot.length - 1, NUM_X_GRID)
+    .map(i => plot[i])
+    .forEach(x => {
+      xData.push(parseInt(x['timestamp']));
+      yData.push(parseFloat(x[indicator]));
+      data.push([parseInt(x['timestamp']), parseFloat(x[indicator])]);
+    });
+
+  const first = yData[0],
+    last = yData[yData.length - 1];
+  const color = COLORS[first > last ? 1 : -1];
+  const yGridRanges = getYScaleRange([height - X_AXIS_HEIGHT, 0], NUM_Y_GRID);
+
+  const xScale = scaleLinear()
+    .domain(extent(xData))
+    .range([Y_AXIS_WIDTH + 10, width - 20]);
+  const xScale1 = xScale.copy().domain([0, xData.length - 1]);
+  const yScale = scaleLinear()
+    .domain(extent(yData))
+    .range([
+      height - X_AXIS_HEIGHT - 20,
+      RECT_HEIGHT + POPUP_PADDING + TRI_HEIGHT,
+    ]);
+  function draw({ cursorX } = {}) {
+    ///////////////// background canvas ///////////////////
+    ctxBg.save();
+    ctxBg.clearRect(0, 0, width, height);
+    ctxBg.beginPath();
+    ctxBg.fillStyle = 'rgba(0,0,0,0.87)';
+    ctxBg.font = '12px';
+    //draw borders
+    ctxBg.strokeStyle = 'rgba(0,0,0,0.12)';
+    ctxBg.moveTo(Y_AXIS_WIDTH, 0);
+    ctxBg.lineTo(Y_AXIS_WIDTH, height - X_AXIS_HEIGHT);
+    ctxBg.lineTo(width, height - X_AXIS_HEIGHT);
+    ctxBg.stroke();
+
+    //draw grid
+    ctxBg.beginPath();
+    ctxBg.setLineDash([2, 1]);
+    const rx = (width + Y_AXIS_WIDTH) / 2;
+    const ry = (height - X_AXIS_HEIGHT) / 2;
+    const gradient = ctxBg.createRadialGradient(rx, ry, 10, rx, ry, rx);
+    gradient.addColorStop(1, 'rgba(249,249,249,0.17)');
+    gradient.addColorStop(0, 'rgba(230,230,230,0.89)');
+    ctxBg.strokeStyle = gradient;
+
+    xData.forEach((_, i) => {
+      const t = xData[i];
+      const x = xScale(t);
+
+      if (X_AXIS_HEIGHT) {
+        ctxBg.save();
+        ctxBg.beginPath();
+        ctxBg.fillStyle = 'rgba(0,0,0,0.87)';
+        ctxBg.textAlign = 'center';
+        ctxBg.textBaseline = 'bottom';
+        ctxBg.translate(x, height - 12);
+        ctxBg.rotate((-50 * Math.PI) / 180);
+        const d = dayjs.unix(t);
+        ctxBg.fillText(d.format('MM/DD'), 0, -5);
+        ctxBg.fillText(d.format('HH:mm'), 0, 5);
+        ctxBg.restore();
+      }
+
+      ctxBg.moveTo(x, 0);
+      ctxBg.lineTo(x, height - X_AXIS_HEIGHT);
+      ctxBg.stroke();
+    });
+
+    yGridRanges.forEach(y => {
+      ctxBg.beginPath();
+      const v = yScale.invert(y);
+      if (Y_AXIS_WIDTH) {
+        ctxBg.save();
+        ctxBg.fillStyle = 'rgba(0,0,0,0.87)';
+        // ctxBg.fillStyle = 'red';
+        ctxBg.textAlign = 'end';
+        ctxBg.textBaseline = 'middle';
+        ctxBg.fillText(format(v), Y_AXIS_WIDTH - 10, y);
+        ctxBg.restore();
+      }
+
+      ctxBg.moveTo(Y_AXIS_WIDTH, y);
+      ctxBg.lineTo(width, y);
+      ctxBg.stroke();
+    });
+
+    ctxBg.restore();
+
+    ///////////////// line canvas ///////////////////
+    ctxLine.clearRect(0, 0, width, height);
+    ctxLine.strokeStyle = color.line;
+    ctxLine.fillStyle = 'rgba(0,0,0,0)';
+    ctxLine.lineWidth = 2;
+
+    ctxLine.beginPath();
+    data.forEach(([t, v], i) => {
+      const x = xScale(t);
+      const y = yScale(v);
+      if (i === 0) {
+        ctxLine.moveTo(x, y);
+      } else {
+        ctxLine.lineTo(x, y);
+      }
+    });
+    ctxLine.stroke();
+    let drawCurrent; //draw after dots to make sure not covered
+    data.forEach(([t, v], i) => {
+      const x = xScale(t);
+      const y = yScale(v);
+
+      if (i === cursorX) {
+        drawCurrent = () => {
+          ctxLine.save();
+
+          //vertical line
+          ctxLine.beginPath();
+          ctxLine.lineWidth = 1;
+          ctxLine.strokeStyle = '#8EB2EB';
+          ctxLine.moveTo(x, 0);
+          ctxLine.lineTo(x, height - X_AXIS_HEIGHT);
+          ctxLine.stroke();
+
+          //rect
+          ctxLine.beginPath();
+          ctxLine.fillStyle = 'rgba(113,143,245,0.8)';
+          const rectTL = Math.min(
+            Math.max(Y_AXIS_WIDTH, x - RECT_WIDTH / 2),
+            width - RECT_WIDTH,
+          );
+          ctxLine.fillRect(
+            rectTL,
+            y - (RECT_HEIGHT + TRI_HEIGHT + POPUP_PADDING),
+            RECT_WIDTH,
+            RECT_HEIGHT,
+          );
+          //triangle
+          ctxLine.beginPath();
+          ctxLine.moveTo(x - TRI_HEIGHT, y - POPUP_PADDING - TRI_HEIGHT - 0.1);
+          ctxLine.lineTo(x, y - POPUP_PADDING);
+          ctxLine.lineTo(x + TRI_HEIGHT, y - POPUP_PADDING - TRI_HEIGHT - 0.1);
+          ctxLine.closePath();
+          ctxLine.fill();
+
+          //text
+          ctxLine.fillStyle = 'white';
+          ctxLine.textAlign = 'left';
+          ctxLine.font = '10px CircularStd';
+          ctxLine.fillText(
+            dayjs.unix(t).format('YYYY/MM/DD HH:mm'),
+            rectTL + 5,
+            y - POPUP_PADDING - TRI_HEIGHT - RECT_HEIGHT * 0.6,
+          );
+          ctxLine.font = 'bold 12px CircularStd';
+          ctxLine.fillText(
+            v.toPrecision(5),
+            rectTL + 5,
+            y - POPUP_PADDING - TRI_HEIGHT - RECT_HEIGHT * 0.2,
+          );
+          //dot
+          ctxLine.beginPath();
+          ctxLine.fillStyle = color.outer;
+          ctxLine.arc(x, y, 6, 0, 2 * Math.PI);
+          ctxLine.fill();
+          ctxLine.beginPath();
+          ctxLine.fillStyle = color.inner;
+          ctxLine.arc(x, y, 4, 0, 12 * Math.PI);
+          ctxLine.fill();
+          ctxLine.restore();
+        };
+      } else {
+        ctxLine.beginPath();
+        ctxLine.clearRect(x - 2, y - 2, 4, 4);
+        // ctxLine.arc(x, y, 2, 0, 2 * Math.PI)
+        if (isSolid) {
+          ctxLine.save();
+          ctxLine.fillStyle = color.line;
+          ctxLine.arc(x, y, 3, 0, 2 * Math.PI);
+          ctxLine.fill();
+          ctxLine.restore();
+        } else {
+          ctxLine.arc(x, y, 2, 0, 2 * Math.PI);
+          ctxLine.stroke();
+        }
+      }
+    });
+    drawCurrent && drawCurrent();
+  }
+  return { xScale1, draw };
+}
+
+function format(v) {
+  if (Math.round(v / 100) > 0) {
+    return Math.round(v);
+  } else {
+    return v.toPrecision(3);
+  }
+}
+
+function range(start, end, num) {
+  const step = Math.round((end - start) / num);
+  const result = [end]; //make sure end in the list but not start
+  let i = end;
+  while ((i -= step) >= start) {
+    result.unshift(i);
+  }
+  return result;
+}
+
+function getYScaleRange(range, num) {
+  const [end, start] = range;
+  const step = (end - start) / num;
+  const result = [end];
+  let i = start;
+  while ((i += step) < end) {
+    result.push(i);
+  }
+  return result;
+}
