3412588034519880f51d6b52e63d038b01719981
chao.zhou <zctocm@gmail.com>
2020-10-14 10:47:24 +0800

    style: format code

--- src/app/containers/Sponsor/index.tsx
+++ src/app/containers/Sponsor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { translations } from '../../../locales/i18n';
