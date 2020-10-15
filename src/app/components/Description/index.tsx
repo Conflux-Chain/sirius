7912b285352dcd027cb768ba0b044f2248149ba2
tangxuezhi <tangxuezhi@conflux-chain.org>
2020-10-15 18:57:26 +0800

    feat(description): add Description component

---
+++ src/app/components/Description/index.tsx
@@ -0,0 +1,81 @@
+/**
+ *
+ * Description
+ *
+ */
+import React from 'react';
+import styled from 'styled-components/macro';
+import { media } from 'styles/media';
+import clsx from 'clsx';
+
+interface Props {
+  title: React.ReactNode;
+  small?: boolean;
+  children: React.ReactNode;
+  noBorder?: boolean;
+}
+type NativeAttrs = Omit<React.HTMLAttributes<any>, keyof Props>;
+export declare type DescriptionProps = React.PropsWithChildren<
+  Props & NativeAttrs
+>;
+
+export const Description = ({
+  title,
+  style,
+  className,
+  children,
+  small,
+  noBorder,
+}: DescriptionProps) => {
+  return (
+    <Wrapper
+      style={style}
+      className={clsx('description', className, {
+        small: small,
+        'no-border': noBorder,
+      })}
+    >
+      <div className="left">{title}</div>
+      <div className="right">{children}</div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  min-height: 3.2857rem;
+  line-height: 3.2857rem;
+  border-bottom: 1px solid #e8e9ea;
+
+  ${media.s} {
+    flex-direction: column;
+  }
+
+  &.no-border {
+    border-bottom: none;
+  }
+
+  &.small {
+    .left {
+      width: 10.8571rem;
+    }
+  }
+
+  .left {
+    padding: 0.8571rem 0;
+    line-height: calc(3.2857rem - 1.7143rem);
+    width: 16.4286rem;
+    color: #002257;
+  }
+
+  .right {
+    padding: 0.8571rem 0;
+    line-height: calc(3.2857rem - 1.7143rem);
+    flex-grow: 1;
+    color: #97a3b4;
+
+    ${media.s} {
+      padding-top: 0;
+    }
+  }
+`;
