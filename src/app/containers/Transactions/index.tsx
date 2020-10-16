6732e4283319b6131ec44cdbc9951d302a57c72d
tangxuezhi <tangxuezhi@conflux-chain.org>
2020-10-16 16:00:45 +0800

    feat(transactions): update todo and issue list

--- src/app/containers/Transactions/index.tsx
+++ src/app/containers/Transactions/index.tsx
@@ -12,7 +12,8 @@ import { useTransactionQuery } from '../../../utils/api';
 import { CopyButton } from '../../components/CopyButton/Loadable';
 import { Link } from 'react-router-dom';
 import SkeletonContainer from '../../components/SkeletonContainer/Loadable';
-import { Status } from '../../components/Status';
+import { Status } from '../../components/Status/Loadable';
+import Tooltip from '../../components/Tooltip';
 
 export const Transactions = () => {
   const { t } = useTranslation();
@@ -27,19 +28,24 @@ export const Transactions = () => {
   if (!data && !error) loading = true;
 
   /**
-   * executed Epoch: ?
-   * token transferred: ?
-   * gas used/limit: 用哪两个值算?
-   * timestamp: todo, need to be formatted
-   * inputData: todo, need to be formatted
-   * to: todo, need to get token info
-   * token transfer: todo, need to get token list
-   * status: need clear code and status map
+   * ISSUE LIST:
+   * - executed Epoch: ? - backend will provide later
+   * - gas used/limit: 用哪两个值算? - miss gas used, backend will provide later
+   * - timestamp: todo, need to be formatted
+   * - inputData: todo, need to be formatted
+   * - to: todo, need to get token info, API: /contract-manager/api/contract/query?<params query>
+   * - token transfer: todo, need to get token list, API: /api/transfer/list?transactionHash=<transaction hash>
+   * - others:
+   *  - CopyButton: 目前是 block 的，后续 react-ui/Tooltip 更新后会解决
+   *  - Status: 图片显示有问题 - 后续 react-ui/Card 更新后会解决
+   *  - Select: 样式不统一 - 后续 Select 会单独封装组件
+   *  - Skeleton: 显示文字 - 后续 react-ui/Skeleton 更新后会解决
+   *  - title tooltip: 需要给定文案后确定哪些需要添加
    */
   const {
     epochHeight,
     timestamp,
-    status,
+    // status,
     from,
     to,
     value,
@@ -65,7 +71,13 @@ export const Transactions = () => {
       <PageHeader>{t(translations.transactions.title)}</PageHeader>
       <StyledCardWrapper>
         <Card className="sirius-Transactions-card">
-          <Description title={t(translations.transactions.hash)}>
+          <Description
+            title={
+              <Tooltip text={t(translations.transactions.hash)} placement="top">
+                {t(translations.transactions.hash)}
+              </Tooltip>
+            }
+          >
             <SkeletonContainer shown={loading}>
               {hash} <CopyButton copyText={hash} />
             </SkeletonContainer>
