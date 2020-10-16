65f6503d0ec9377619875e57e05f64b96f4cd604
Angelia <angelia@Angelia-MacBook-Pro.local>
2020-10-17 00:10:45 +0800

    test: update snapshot

--- src/app/containers/TokenDetail/index.tsx
+++ src/app/containers/TokenDetail/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import styled from 'styled-components/macro';
+// import styled from 'styled-components/macro';
 import { useTranslation } from 'react-i18next';
 import { translations } from '../../../locales/i18n';
 import { List } from '../../components/List/Loadable';
