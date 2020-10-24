f60496219f0a6e133f20c8a1c7708967ee377e2b
Angelia <angelia@Angelia-MacBook-Pro.local>
2020-10-24 18:33:34 +0800

    fix: switch to mainnet

--- src/utils/hooks/useTestnet.ts
+++ src/utils/hooks/useTestnet.ts
@@ -11,8 +11,8 @@ const isTestEnv = () => window.location.hostname.includes('scantest');
 
 export const toTestnet = () => {
   if (isTestEnv())
-    return window.location.assign('//testnet.scantest.confluxscan.io');
-  return window.location.assign('//testnet.confluxscan.io');
+    return window.location.assign('//testnet-scantest.confluxnetwork.org');
+  return window.location.assign('//scantest.confluxnetwork.org');
 };
 export const toMainnet = () => {
   if (isTestEnv()) return window.location.assign('//scantest.confluxscan.io');
