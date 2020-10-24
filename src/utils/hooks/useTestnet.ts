4895d85cd1b3d2dc4f5099982c8fd8a7ac89a6d6
Angelia <angelia@Angelia-MacBook-Pro.local>
2020-10-24 19:36:12 +0800

    fix: bugs

--- src/utils/hooks/useTestnet.ts
+++ src/utils/hooks/useTestnet.ts
@@ -12,9 +12,10 @@ const isTestEnv = () => window.location.hostname.includes('scantest');
 export const toTestnet = () => {
   if (isTestEnv())
     return window.location.assign('//testnet-scantest.confluxnetwork.org');
-  return window.location.assign('//scantest.confluxnetwork.org');
+  return window.location.assign('//testnet.confluxscan.io');
 };
 export const toMainnet = () => {
-  if (isTestEnv()) return window.location.assign('//scantest.confluxscan.io');
+  if (isTestEnv())
+    return window.location.assign('//scantest.confluxnetwork.org');
   return window.location.assign('//confluxscan.io');
 };
