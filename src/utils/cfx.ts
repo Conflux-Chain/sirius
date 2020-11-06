97e0bf6ddb114a4c1f99cea07f45de7fe02b1f4b
chao.zhou <zctocm@gmail.com>
2020-11-06 15:27:13 +0800

    feat(sponsor): add testnet faucet

--- src/utils/cfx.ts
+++ src/utils/cfx.ts
@@ -3,12 +3,18 @@ import {
   util as cfxUtil,
 } from 'js-conflux-sdk/dist/js-conflux-sdk.umd.min.js';
 import { Faucet } from 'conflux-sponsorfaucet';
+import { isTestNetEnv } from './hooks/useTestnet';
 const cfxUrl = window.location.origin + '/rpc';
 
 const cfx = new Conflux({
   url: cfxUrl,
 });
-const faucetAddress = '0x8d5adbcaf5714924830591586f05302bf87f74bd';
+const mainnetFaucetAddress = '0x8d5adbcaf5714924830591586f05302bf87f74bd';
+const testnetFaucetAddress = '0x8097e818c2c2c1524c41f0fcbda143520046d117';
+const faucetAddress = isTestNetEnv()
+  ? testnetFaucetAddress
+  : mainnetFaucetAddress;
+console.log(faucetAddress);
 const faucet = new Faucet(cfxUrl, faucetAddress);
 export const decodeContract = ({ abi, address, transacionData }) => {
   const contract = cfx.Contract({ abi, address });
