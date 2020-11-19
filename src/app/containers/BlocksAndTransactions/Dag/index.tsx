610b51f6d2697b071fba5028e48ebfd1d54b24a3
Guichi Zhao <zgc910619@gmail.com>
2020-11-19 18:05:33 +0800

    feat: Dag (#180)
    * feat(dag): new dag
    
    * fix(dag): fix hard code width
    
    * fix(commit): commmit mesasage
    
    * fix(dag): infinate loop
    
    * fix: now.json, remove old dag bulid script
    
    Co-authored-by: guichi <guichi.zhao@zerologix.com>
    Co-authored-by: yqrashawn <namy.19@gmail.com>

--- src/app/containers/BlocksAndTransactions/Dag/index.tsx
+++ src/app/containers/BlocksAndTransactions/Dag/index.tsx
@@ -1,63 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { useTranslation } from 'react-i18next';
-import { translations } from '../../../../locales/i18n';
-import { DagGraph } from './Dag';
-import { useBreakpoint } from 'styles/media';
-import SkeletonContainer from '../../../components/SkeletonContainer/Loadable';
+import dag from './lib-dag.js';
 
 export function Dag() {
-  const { t } = useTranslation();
-  const bp = useBreakpoint();
-  const [loading, setLoading] = useState<boolean>(true);
-
-  return (
-    <>
-      {bp !== 's' && [
-        loading && (
-          <SkeletonContainer key="skeleton" shown={true}>
-            <FakeDag />
-          </SkeletonContainer>
-        ),
-        <DagContainer key="dag" isLoading={loading}>
-          <DagGraph onLoaded={() => setLoading(false)}>
-            <DagTitle isLoading={loading}>
-              {t(translations.block.title)}
-            </DagTitle>
-          </DagGraph>
-        </DagContainer>,
-      ]}
-    </>
-  );
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (ref.current !== null) {
+      const { width, height } = ref.current.getBoundingClientRect();
+      let destory = dag({ container: ref.current, width, height });
+      const visibilityChange = () => {
+        if (document.hidden) {
+          destory();
+        } else {
+          destory = dag({
+            container: ref.current,
+            width: width,
+            height: height,
+          });
+        }
+      };
+      document.addEventListener('visibilitychange', visibilityChange);
+      return () => {
+        destory();
+        document.removeEventListener('visibilitychange', visibilityChange);
+      };
+    }
+  }, []);
+  return <Container ref={ref}>DAG</Container>;
 }
 
-const FakeDag = styled.div`
-  min-height: 17.1429rem;
+const Container = styled.div`
   width: 100%;
-  border-radius: 0.2857rem;
-`;
-
-const DagContainer = styled.div<{ isLoading: boolean }>`
-  position: ${props => (props.isLoading ? 'absolute' : 'relative')};
-  margin-top: 1.71rem;
-  margin-bottom: 1.14rem;
-  width: 100%;
-  min-height: 17.1429rem;
-  #dag-viewer {
-    min-height: 17.1429rem;
-    width: 100%;
-    > canvas {
-      border-radius: 0.2857rem;
-    }
-  }
-`;
-const DagTitle = styled.div<{ isLoading: boolean }>`
-  display: ${props => (props.isLoading ? 'none' : 'block')};
-  position: absolute;
-  top: 1rem;
-  left: 2rem;
-  font-size: 1.14rem;
-  font-weight: bold;
-  // color: #0b132e;
-  color: white;
+  height: 127px;
+  margin-top: 24px;
+  background-color: #eceff8;
+  box-shadow: 12px 8px 24px -12px rgba(20, 27, 50, 0.12);
 `;
