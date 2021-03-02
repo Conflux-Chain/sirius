98242a4d6e022ca4447a6f414c2d2e36154c725f
jnoodle <fullstack@126.com>
2021-03-02 16:41:00 +0800

    feat: SCAN-567 countdown show full time tooltip

--- src/app/components/CountDown/index.tsx
+++ src/app/components/CountDown/index.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { translations } from '../../../locales/i18n';
 import { getDuration } from '../../../utils';
+import { Text } from '../Text/Loadable';
 
 interface CountDownProps {
   from: number;
@@ -27,7 +28,6 @@ export const CountDown = ({
     translations.general.countdown.second,
   ];
 
-  console.log(duration);
   const label = duration.reduce(
     (acc, cur, index) => {
       // default max to retain 2 duration, and min to retain 1 duration
@@ -41,9 +41,21 @@ export const CountDown = ({
     ['', 0],
   );
 
+  const fullLabel = duration.reduce(
+    (acc, cur, index) => {
+      if (acc[1] < 4 && (cur > 0 || index === 3)) {
+        const next = t(str[index], { count: cur });
+        acc[0] = acc[0] ? `${acc[0]} ${next}` : `${next}`;
+        acc[1] = Number(acc[1]) + 1;
+      }
+      return acc;
+    },
+    ['', 0],
+  );
+
   return (
-    <>
+    <Text span hoverValue={fullLabel[0]}>
       {label[0]} {t(translations.general.countdown.ago)}
-    </>
+    </Text>
   );
 };
