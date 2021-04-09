f4e302a5aca5165542be6d6dcaffa640fd8ea7e8
jnoodle <fullstack@126.com>
2021-04-09 20:16:02 +0800

    fix: SCAN-806 tokens to token

--- src/app/containers/Header/index.tsx
+++ src/app/containers/Header/index.tsx
@@ -97,7 +97,7 @@ export const Header = memo(() => {
         {
           // erc 20
           title: [
-            t(translations.header.tokens20).replace('Token', 'Tokens'),
+            t(translations.header.tokens20),
             <Check size={18} key="check" />,
           ],
           name: ScanEvent.menu.action.tokens20,
@@ -106,7 +106,7 @@ export const Header = memo(() => {
         {
           // erc 721
           title: [
-            t(translations.header.tokens721).replace('Token', 'Tokens'),
+            t(translations.header.tokens721),
             <Check size={18} key="check" />,
           ],
           name: ScanEvent.menu.action.tokens721,
@@ -115,7 +115,7 @@ export const Header = memo(() => {
         {
           // erc 1155
           title: [
-            t(translations.header.tokens1155).replace('Token', 'Tokens'),
+            t(translations.header.tokens1155),
             <Check size={18} key="check" />,
           ],
           name: ScanEvent.menu.action.tokens1155,
