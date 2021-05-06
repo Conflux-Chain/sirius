6d694d804b645bc04c194a75d8228d916d7525d9
tangxuezhi <tangxuezhi@conflux-chain.org>
2021-05-06 14:52:55 +0800

    feat: SCAN-1058, update mined blocks table list

--- src/app/containers/AddressContractDetail/Table.tsx
+++ src/app/containers/AddressContractDetail/Table.tsx
@@ -20,25 +20,26 @@ import {
 import { StyledIconWrapper } from 'utils/tableColumns/token';
 import { Link } from 'app/components/Link';
 import { Text } from 'app/components/Text';
-import { ColumnsType } from '../../components/TabsTablePanel';
+import { ColumnsType } from 'app/components/TabsTablePanel';
 import { formatType } from 'js-conflux-sdk/src/contract/abi';
 import {
   TabLabel,
   TabsTablePanel,
-} from '../../components/TabsTablePanel/Loadable';
+} from 'app/components/TabsTablePanel/Loadable';
 import { formatString, isContractAddress, isInnerContractAddress } from 'utils';
 import { media, useBreakpoint } from 'styles/media';
 import { defaultTokenIcon } from '../../../constants';
 import {
   TableSearchDatepicker,
   TableSearchDropdown,
-} from '../../components/TablePanel';
+} from 'app/components/TablePanel';
 import { cfx } from 'utils/cfx';
-import { ContractAbi } from '../../components/ContractAbi/Loadable';
-import { cfxTokenTypes } from '../../../utils/constants';
-import { trackEvent } from '../../../utils/ga';
-import { ScanEvent } from '../../../utils/gaConstants';
-import { useAge } from '../../../utils/hooks/useAge';
+import { ContractAbi } from 'app/components/ContractAbi/Loadable';
+import { cfxTokenTypes } from 'utils/constants';
+import { trackEvent } from 'utils/ga';
+import { ScanEvent } from 'utils/gaConstants';
+import { useAge } from 'utils/hooks/useAge';
+import { AddressContainer } from 'app/components/AddressContainer';
 
 const AceEditorStyle = {
   width: '100%',
@@ -458,7 +459,10 @@ export function Table({ address, addressInfo }) {
     blockColunms.position,
     blockColunms.txns,
     blockColunms.hash,
-    blockColunms.miner,
+    {
+      ...blockColunms.miner,
+      render: value => <AddressContainer isLink={false} value={value} />,
+    },
     blockColunms.avgGasPrice,
     blockColunms.gasUsedPercent,
     blockColunms.reward,
