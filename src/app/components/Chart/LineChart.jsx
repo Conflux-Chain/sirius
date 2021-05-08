d05a651f576439de8caf26d291fd1229276a76b8
jnoodle <fullstack@126.com>
2021-05-08 10:08:40 +0800

    feat: hide transferAmount for data issue

--- src/app/components/Chart/LineChart.jsx
+++ src/app/components/Chart/LineChart.jsx
@@ -300,7 +300,7 @@ export const LineChart = ({
         return 'contractCount';
       case 'tokenAnalysis':
         return [
-          'transferAmount',
+          // 'transferAmount',
           'transferCount',
           'uniqueReceiver',
           'uniqueSender',
