58f006305680b4f3d030898b61572f596fc33dfa
tangxuezhi <0x74616e67@gmail.com>
2021-06-16 16:25:47 +0800

    fix: SCAN-1615, use monospaceFont for general decode of inputdata

--- src/app/components/TxnComponents/InputData/GeneralDecode.tsx
+++ src/app/components/TxnComponents/InputData/GeneralDecode.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Wrapper } from './Common';
 import _ from 'lodash';
+import { monospaceFont } from 'styles/variable';
+import styled from 'styled-components/macro';
 
 export const GeneralDecode = ({ data = '', decodedData }) => {
   const { fullName } = decodedData;
@@ -8,22 +10,28 @@ export const GeneralDecode = ({ data = '', decodedData }) => {
 
   return (
     <Wrapper>
-      {fullName ? (
-        <>
-          <div className="general-decode-fullname">Function: {fullName}</div>
-          <br />
-        </>
-      ) : (
-        ''
-      )}
-      <div className="general-decode-signature">
-        MethodID: {data.slice(0, 10)}
-      </div>
-      <div className="general-decode-signature">
-        {originalDataSlice.map((o, index) => {
-          return <div key={index}>{`[${index}]: ${o}`}</div>;
-        })}
-      </div>
+      <StyledBodyWrapper>
+        {fullName ? (
+          <>
+            <div className="general-decode-fullname">Function: {fullName}</div>
+            <br />
+          </>
+        ) : (
+          ''
+        )}
+        <div className="general-decode-signature">
+          MethodID: {data.slice(0, 10)}
+        </div>
+        <div className="general-decode-signature">
+          {originalDataSlice.map((o, index) => {
+            return <div key={index}>{`[${index}]: ${o}`}</div>;
+          })}
+        </div>
+      </StyledBodyWrapper>
     </Wrapper>
   );
 };
+
+const StyledBodyWrapper = styled.div`
+  font-family: ${monospaceFont};
+`;
