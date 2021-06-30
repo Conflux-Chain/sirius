b61e8d351f6e23f90d49891b14efdd38bd59dd5a
tangxuezhi <0x74616e67@gmail.com>
2021-06-30 13:06:22 +0800

    feat: SCAN-1733, add token type tag to transfer list of txn detail

--- src/app/components/TxnComponents/index.tsx
+++ src/app/components/TxnComponents/index.tsx
@@ -4,4 +4,5 @@ export { StorageFee } from './StorageFee';
 export { Nonce } from './Nonce';
 export { TokenTransfer } from './TokenTransfer';
 export { Overview } from './Overview';
+export { TokenTypeTag } from './TokenTypeTag';
 export { InputData as InputDataNew } from './InputData';
