84a79ec739e1e43e38253445d4f93cc86bd5a3eb
tangxuezhi <0x74616e67@gmail.com>
2021-07-09 11:13:13 +0800

    feat: update txns&refer blocks table in block detail

---
+++ src/app/containers/Block/Txns.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TablePanel as TablePanelNew } from 'app/components/TablePanelNew';
+import { transactionColunms } from 'utils/tableColumns';
+import { useAge } from 'utils/hooks/useAge';
+import { TablePanel } from 'app/components/TablePanel/Loadable';
+
+interface Props {
+  url: string;
+}
+
+export const Txns = ({ url }: Props) => {
+  const [ageFormat, toggleAgeFormat] = useAge();
+
+  const columnsWidth = [4, 6, 6, 4, 3, 4, 5];
+  const columns = [
+    transactionColunms.hash,
+    transactionColunms.from,
+    transactionColunms.to,
+    transactionColunms.value,
+    transactionColunms.gasFee,
+    transactionColunms.gasPrice,
+    transactionColunms.age(ageFormat, toggleAgeFormat),
+  ].map((item, i) => ({ ...item, width: columnsWidth[i] }));
+
+  return (
+    <>
+      <TablePanelNew url={url} columns={columns} rowKey="hash"></TablePanelNew>
+
+      {/* @todo, table-refactor, need to remove */}
+      <TablePanel url={url} table={{ columns: columns, rowKey: 'hash' }} />
+    </>
+  );
+};
--- src/app/containers/Block/index.tsx
+++ src/app/containers/Block/index.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { translations } from 'locales/i18n';
-import { ColumnsType } from 'app/components/TabsTablePanel';
 import { Tooltip } from 'app/components/Tooltip/Loadable';
 import {
   TabLabel,
   TabsTablePanel,
 } from 'app/components/TabsTablePanel/Loadable';
-import { blockColunms, transactionColunms } from 'utils/tableColumns';
-import { useAge } from 'utils/hooks/useAge';
 import { useParams } from 'react-router-dom';
 import { PageHeader } from 'app/components/PageHeader/Loadable';
 import { Helmet } from 'react-helmet-async';
@@ -17,13 +14,15 @@ import styled from 'styled-components/macro';
 import { reqBlockDetail } from 'utils/httpRequest';
 import { useBreakpoint } from 'styles/media';
 
+import { Txns } from './Txns';
+import { ReferenceBlocks } from './ReferenceBlocks';
+
 export function Block() {
   const bp = useBreakpoint();
   const { t } = useTranslation();
   const { hash } = useParams<{
     hash: string;
   }>();
-  const [ageFormat, toggleAgeFormat] = useAge();
   const [{ transactionCount, refereeHashes }, setBlockDetail] = useState<any>({
     transactionCount: 0,
     refereeHashes: [],
@@ -37,29 +36,6 @@ export function Block() {
     });
   }, [hash]);
 
-  const columnsTransactionsWidth = [4, 6, 6, 4, 3, 4, 5];
-  const columnsTransactions: ColumnsType = [
-    transactionColunms.hash,
-    transactionColunms.from,
-    transactionColunms.to,
-    transactionColunms.value,
-    transactionColunms.gasFee,
-    transactionColunms.gasPrice,
-    transactionColunms.age(ageFormat, toggleAgeFormat),
-  ].map((item, i) => ({ ...item, width: columnsTransactionsWidth[i] }));
-
-  const columnsBlocksWidth = [4, 2, 2, 4, 6, 3, 5, 5];
-  const columnsBlocks: ColumnsType = [
-    blockColunms.epoch,
-    blockColunms.position,
-    blockColunms.txns,
-    blockColunms.hash,
-    blockColunms.miner,
-    blockColunms.difficulty,
-    blockColunms.gasUsedPercentWithProgress,
-    blockColunms.age(ageFormat, toggleAgeFormat),
-  ].map((item, i) => ({ ...item, width: columnsBlocksWidth[i] }));
-
   const tabs = [
     {
       value: 'overview',
@@ -67,13 +43,13 @@ export function Block() {
       content: <DescriptionPanel />,
     },
     {
-      value: 'blocks',
+      value: 'transactions',
       action: 'blockTransactions',
-      label: (total: number, realTotal: number) => {
+      label: () => {
         return (
           <TabLabel
-            total={transactionCount || total}
-            realTotal={transactionCount || realTotal}
+            total={transactionCount}
+            realTotal={transactionCount}
             showTooltip={bp !== 's'}
           >
             {bp === 's' ? (
@@ -89,23 +65,15 @@ export function Block() {
           </TabLabel>
         );
       },
-      url: `/transaction?blockHash=${hash}`,
-      table: {
-        columns: columnsTransactions,
-        rowKey: 'hash',
-      },
+      content: <Txns url={`/transaction?blockHash=${hash}`} />,
       hidden: !transactionCount,
     },
     {
-      value: 'transaction',
-      action: 'referenceBlocks',
-      label: (total: number, realTotal: number) => {
+      value: 'reference-blocks',
+      action: 'blockTransactions',
+      label: () => {
         return (
-          <TabLabel
-            total={total}
-            realTotal={realTotal}
-            showTooltip={bp !== 's'}
-          >
+          <TabLabel total={refereeHashes?.length} showTooltip={bp !== 's'}>
             {bp === 's' ? (
               t(translations.block.tabs.referenceBlocks)
             ) : (
@@ -119,12 +87,7 @@ export function Block() {
           </TabLabel>
         );
       },
-      hideTotalZero: true,
-      url: `/block?referredBy=${hash}`,
-      table: {
-        columns: columnsBlocks,
-        rowKey: 'hash',
-      },
+      content: <ReferenceBlocks url={`/block?referredBy=${hash}`} />,
       hidden: !refereeHashes?.length,
     },
   ];
