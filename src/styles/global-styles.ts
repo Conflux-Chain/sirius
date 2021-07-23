1885080f09b1c03ce399aefa8a02b477135866f3
tangxuezhi <0x74616e67@gmail.com>
2021-07-23 10:49:24 +0800

    feat: add box-shadow to table

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -175,7 +175,22 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   .ant-table-title {
-    border-bottom: 1px solid rgb(232, 233, 234)
+    border-bottom: 1px solid rgb(232, 233, 234);
+    padding: 16px 0;
+  }
+
+  .ant-table-wrapper.shadowed {
+    .ant-table {
+      box-shadow: rgb(20 27 50 / 12%) 0.8571rem 0.5714rem 1.7143rem -0.8571rem;
+      padding: 0px 1.2857rem;
+      border-radius: 4px;
+    }
+  }
+
+  .ant-pagination-options-quick-jumper {
+    input {
+      margin-right: 0;
+    } 
   }
 
   ul li:before {
