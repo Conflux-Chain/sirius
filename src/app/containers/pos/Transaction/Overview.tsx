e77b5931a5b592a2bc3760a7d968dc03a3da62c5
tangxuezhi <0x74616e67@gmail.com>
2021-11-19 16:25:05 +0800

    feat: SCAN-3346, add tx payload module

--- src/app/containers/pos/Transaction/Overview.tsx
+++ src/app/containers/pos/Transaction/Overview.tsx
@@ -8,9 +8,82 @@ import SkeletonContainer from 'app/components/SkeletonContainer/Loadable';
 import { formatTimeStamp } from 'utils';
 import lodash from 'lodash';
 import { Link } from 'app/components/Link/Loadable';
+import styled from 'styled-components/macro';
+import { media } from 'styles/media';
+import { Typography } from '@cfxjs/antd';
+import SDK from 'js-conflux-sdk/dist/js-conflux-sdk.umd.min.js';
+import { PoSAddressContainer } from 'app/components/AddressContainer/Loadable';
+
+const { Text } = Typography;
 
 export function Overview({ data, loading }) {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const lang = i18n.language.includes('zh') ? 'zh' : 'en';
+
+  let payloadInfoItems: Array<{
+    label: React.ReactNode;
+    content: React.ReactNode;
+  }> = [];
+
+  try {
+    if (!lodash.isNil(data.payload)) {
+      payloadInfoItems = Object.keys(data.payload).map(p => {
+        let label: React.ReactNode;
+        let content: React.ReactNode;
+
+        if (p === 'conflictingVotes') {
+          label = `${t(translations.pos.common.txPayload.conflictingVotes)}`;
+
+          try {
+            content = `${t(
+              translations.pos.common.txPayload.conflictVoteType,
+            )}: ${t(
+              translations.pos.common.txPayload[
+                data.payload[p].conflictVoteType
+              ] || '--',
+            )}, ${t(translations.pos.common.txPayload.first)}: ${
+              data.payload[p].first
+            }, ${t(translations.pos.common.txPayload.second)}: ${
+              data.payload[p].second
+            }`;
+          } catch (e) {}
+        } else {
+          label = t(translations.pos.common.txPayload[p]) || p;
+          content = data.payload[p];
+        }
+
+        if (lodash.isNil(content)) {
+          content = '--';
+        } else if (['targetTerm', 'votingPower', 'height'].includes(p)) {
+          content = SDK.format.uInt(content);
+        } else if (p === 'address') {
+          content = (
+            <>
+              <PoSAddressContainer
+                value={content as string}
+                isFull={true}
+              ></PoSAddressContainer>{' '}
+              <CopyButton copyText={content as string} />
+            </>
+          );
+        } else if (p === 'blockHash') {
+          content = (
+            <>
+              <Link href={`/block/${content}`}>{content}</Link>{' '}
+              <CopyButton copyText={content as string} />
+            </>
+          );
+        }
+
+        return {
+          label: label,
+          content: content,
+        };
+      });
+    }
+  } catch (e) {
+    console.log('payload error: ', e);
+  }
 
   return (
     <Card>
@@ -52,7 +125,7 @@ export function Overview({ data, loading }) {
             '--'
           ) : (
             <>
-              {data.number} <CopyButton copyText={data.number} />
+              {data.number} <CopyButton copyText={String(data.number)} />
             </>
           )}
         </SkeletonContainer>
@@ -71,34 +144,100 @@ export function Overview({ data, loading }) {
           )}
         </SkeletonContainer>
       </Description>
-      <Description
-        title={t(translations.pos.transaction.overview.blockNumber)}
-        noBorder
-      >
+      <Description title={t(translations.pos.transaction.overview.blockNumber)}>
         <SkeletonContainer shown={loading}>
           {lodash.isNil(data.blockNumber) ? (
             '--'
           ) : (
             <>
-              {data.blockNumber} <CopyButton copyText={data.blockNumber} />
+              {data.blockNumber}{' '}
+              <CopyButton copyText={String(data.blockNumber)} />
             </>
           )}
         </SkeletonContainer>
       </Description>
-      {/* <Description
+      <Description
         title={t(translations.pos.transaction.overview.payload)}
         noBorder
       >
         <SkeletonContainer shown={loading}>
-          {lodash.isNil(data.blockNumber) ? (
+          {!payloadInfoItems.length ? (
             '--'
           ) : (
-            <>
-              {data.blockNumber} <CopyButton copyText={data.blockNumber} />
-            </>
+            <StyledPayloadWrapper>
+              <DescriptionInline
+                data={payloadInfoItems}
+                type={data.type}
+                labelWidth={lang === 'zh' ? 90 : 180}
+              ></DescriptionInline>
+            </StyledPayloadWrapper>
           )}
         </SkeletonContainer>
-      </Description> */}
+      </Description>
     </Card>
   );
 }
+
+const StyledPayloadWrapper = styled.div`
+  background-color: rgb(247, 247, 248);
+  margin: -0.3571rem;
+  padding: 0.3571rem;
+`;
+
+interface DescriptionInlinePropsType {
+  data: Array<{
+    key?: string | number;
+    label: React.ReactNode;
+    content: React.ReactNode;
+  }>;
+  type: string;
+  labelWidth?: number;
+}
+const DescriptionInline = ({
+  data,
+  type,
+  labelWidth = 100,
+}: DescriptionInlinePropsType) => {
+  return (
+    <StyledDescriptionInlineWrapper labelWidth={labelWidth}>
+      {data.map((d, i) => {
+        return (
+          <div className="description-inline-item" key={d.key || i}>
+            <Text
+              className="description-inline-item-label"
+              ellipsis={{
+                suffix: ':',
+                tooltip: d.label,
+              }}
+            >
+              {d.label}
+            </Text>{' '}
+            <span className="description-inline-item-content">{d.content}</span>
+          </div>
+        );
+      })}
+    </StyledDescriptionInlineWrapper>
+  );
+};
+
+const StyledDescriptionInlineWrapper = styled.div<{
+  labelWidth?: number;
+}>`
+  .description-inline-item {
+    display: flex;
+
+    ${media.s} {
+      flex-direction: column;
+      margin-bottom: 0.4286rem;
+    }
+
+    .description-inline-item-label {
+      width: ${props => `${props.labelWidth}px`};
+      flex-shrink: 0;
+    }
+
+    .description-inline-item-content {
+      flex-grow: 1;
+    }
+  }
+`;
