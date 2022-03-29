3c3b12fe251bbc86534f01f81e82b25f1c95f3ed
tangxuezhi <0x74616e67@gmail.com>
2022-03-29 15:04:14 +0800

    feat: update release notices and version

--- src/index.tsx
+++ src/index.tsx
@@ -61,7 +61,7 @@ Promise.all([completeDetect()]).then(() => {
   render(App);
 });
 
-const currentVersion = 'V2.7.0';
+const currentVersion = 'V2.8.0';
 
 const brand = `
 ┌─┐┌─┐┌┐┌┌─┐┬  ┬ ┬─┐ ┬  ┌─┐┌─┐┌─┐┌┐┌ ${currentVersion}
