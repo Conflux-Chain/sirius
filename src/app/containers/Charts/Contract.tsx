be6e069b45f609008b2f710d0123d8b9f9120395
tangxuezhi <0x74616e67@gmail.com>
2022-05-17 14:06:41 +0800

    feat: change data limit to 10000 of token/contract charts

--- src/app/containers/Charts/Contract.tsx
+++ src/app/containers/Charts/Contract.tsx
@@ -25,7 +25,7 @@ export function Contract({
       url: OPEN_API_URLS.contract,
       query: {
         address,
-        limit: 100,
+        limit: 10000,
       },
       formatter: data => {
         const data1: any = [];
--- src/app/containers/Charts/Token.tsx
+++ src/app/containers/Charts/Token.tsx
@@ -27,7 +27,7 @@ export function Token({
       url: OPEN_API_URLS.token,
       query: {
         base32: address,
-        limit: 100,
+        limit: 10000,
       },
       formatter: data => {
         const data1: any = [];
