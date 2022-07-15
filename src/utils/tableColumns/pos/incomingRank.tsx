fa0c9476b8f46e5305293d33a8c3602f704069ff
tangxuezhi <0x74616e67@gmail.com>
2022-07-15 11:28:41 +0800

    feat: update style of pos income rank

--- src/utils/tableColumns/pos/incomingRank.tsx
+++ src/utils/tableColumns/pos/incomingRank.tsx
@@ -5,6 +5,8 @@ import { ContentWrapper } from '../utils';
 import lodash from 'lodash';
 import { fromDripToCfx } from 'utils';
 import { Text } from 'app/components/Text/Loadable';
+import { PlusOutlined } from '@ant-design/icons';
+import styled from 'styled-components/macro';
 
 export const totalIncoming = {
   title: (
@@ -52,7 +54,7 @@ export const day = (number: number | 'all') => {
   const dataIndex = number === 'all' ? 'all' : `day${number}`;
   return {
     title: (
-      <ContentWrapper>
+      <ContentWrapper right>
         {number === 'all' ? (
           <Translation>
             {t => t(translations.pos.incomingRank.rewardRank.all)}
@@ -71,7 +73,26 @@ export const day = (number: number | 'all') => {
       const cfx = fromDripToCfx(
         number === 'all' ? row.accountInfo?.totalReward : row[`day${number}`],
       );
-      return <ContentWrapper>{cfx}</ContentWrapper>;
+
+      return (
+        <ContentWrapper right>
+          <StyledDayWrapper>
+            {cfx !== '0' && <PlusOutlined className="icon" />}
+            {cfx}
+          </StyledDayWrapper>
+        </ContentWrapper>
+      );
     },
   };
 };
+
+const StyledDayWrapper = styled.div`
+  display: inline-flex;
+  align-items: center;
+
+  .icon {
+    font-size: 10px;
+    font-weight: bold;
+    margin-right: 1px;
+  }
+`;
