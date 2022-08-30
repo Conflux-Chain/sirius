d7d66093e5b83995df707d4e6db3196a28e4bc2e
tangxuezhi <0x74616e67@gmail.com>
2022-08-30 18:16:48 +0800

    fix: hotfix for pending reason issue

--- src/app/containers/Notices/index.tsx
+++ src/app/containers/Notices/index.tsx
@@ -12,8 +12,8 @@ export const noticeInfo = {
   hot: false,
   type: 'Announcement',
   brief: {
-    en: 'ConfluxScan v2.16.0 Released!',
-    zh: 'ConfluxScan v2.16.0 发布喽！',
+    en: 'ConfluxScan v2.16.1 Released!',
+    zh: 'ConfluxScan v2.16.1 发布喽！',
   },
   link: {
     en:
