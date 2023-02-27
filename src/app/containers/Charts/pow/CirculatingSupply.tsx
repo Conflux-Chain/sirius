d1a53fcc955db7d0cd9923a5cd09a549cbf9cafd
0x74616e67 <0x74616e67@gmail.com>
2023-02-27 15:01:26 +0800

    feat: update supply charts statistical algorithm

--- src/app/containers/Charts/pow/CirculatingSupply.tsx
+++ src/app/containers/Charts/pow/CirculatingSupply.tsx
@@ -21,22 +21,30 @@ export function CirculatingSupply({ preview = false }: ChildProps) {
         if (data) {
           return [
             {
+              sliced: true,
+              selected: true,
               name: t(translations.highcharts.pow.circulatingSupply.others),
               y: parseInt(
                 new SDK.Drip(
                   new BigNumber(data?.totalCirculating)
                     .minus(data?.nullAddressBalance)
+                    .minus(data?.totalStaking)
+                    .minus(data?.totalCollateral)
                     .toNumber(),
                 ).toCFX(),
               ),
             },
             {
-              sliced: true,
-              selected: true,
               name: t(
-                translations.highcharts.pow.circulatingSupply.zeroAddress,
+                translations.highcharts.pow.circulatingSupply.totalCollateral,
               ),
-              y: parseInt(new SDK.Drip(data?.nullAddressBalance).toCFX()),
+              y: parseInt(new SDK.Drip(data?.totalCollateral).toCFX()),
+            },
+            {
+              name: t(
+                translations.highcharts.pow.circulatingSupply.totalStaking,
+              ),
+              y: parseInt(new SDK.Drip(data?.totalStaking).toCFX()),
             },
           ];
         }
