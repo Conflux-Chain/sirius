0892211f34321ff8cd3b6c621d89654905fea247
0x74616e67 <0x74616e67@gmail.com>
2023-08-04 10:49:42 +0800

    feat: handle nonstandard attributes of NFT metadata

--- src/app/containers/NFTDetail/index.tsx
+++ src/app/containers/NFTDetail/index.tsx
@@ -225,13 +225,15 @@ export function NFTDetail(props) {
     let dateTypeAttributes: any = [],
       stringTypeAttributes: any = [];
 
-    attributes?.forEach(a => {
-      if (a.display_type === 'date') {
-        dateTypeAttributes.push(a);
-      } else if (a.display_type === undefined) {
-        stringTypeAttributes.push(a);
-      }
-    });
+    attributes
+      ?.filter(a => a.trait_type !== undefined && a.value !== undefined)
+      .forEach(a => {
+        if (a.display_type === 'date') {
+          dateTypeAttributes.push(a);
+        } else if (a.display_type === undefined) {
+          stringTypeAttributes.push(a);
+        }
+      });
 
     return {
       description,
