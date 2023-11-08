93cbdfe3744f6113e86457fe21decedcf3bfd329
shine <shouhui.sun@gmail.com>
2023-11-08 17:04:25 +0800

    fix: metadata fault tolerance processing

--- src/app/containers/NFTDetail/index.tsx
+++ src/app/containers/NFTDetail/index.tsx
@@ -17,6 +17,9 @@ import { useBreakpoint } from 'styles/media';
 import { InfoIconWithTooltip } from 'app/components/InfoIconWithTooltip/Loadable';
 import { Button } from 'app/components/Button/Loadable';
 import { usePlatform } from 'utils/hooks/usePlatform';
+import SDK from 'js-conflux-sdk/dist/js-conflux-sdk.umd.min.js';
+import { abi as ERC1155ABI } from 'utils/contract/ERC1155.json';
+import { abi as ERC721ABI } from 'utils/contract/ERC721.json';
 
 import AceEditor from 'react-ace';
 import 'ace-builds/webpack-resolver';
@@ -24,6 +27,7 @@ import 'ace-builds/src-noconflict/mode-json';
 import 'ace-builds/src-noconflict/theme-tomorrow';
 
 import { formatTimeStamp, addIPFSGateway } from 'utils';
+import { RPC_SERVER, NETWORK_ID } from 'utils/constants';
 
 import { TransferAndHolders } from './TransferAndHolders';
 import { TransferModal } from './TransferModal';
@@ -175,6 +179,45 @@ export function NFTDetail(props) {
     symbol: '',
   });
 
+  const contract721 = useMemo(() => {
+    const CFX = new SDK.Conflux({
+      url: RPC_SERVER,
+      networkId: NETWORK_ID,
+    });
+    return CFX.Contract({
+      address: address,
+      abi: ERC721ABI,
+    });
+  }, [address]);
+
+  const contract1155 = useMemo(() => {
+    const CFX = new SDK.Conflux({
+      url: RPC_SERVER,
+      networkId: NETWORK_ID,
+    });
+    return CFX.Contract({
+      address: address,
+      abi: ERC1155ABI,
+    });
+  }, [address]);
+
+  const reqNFTContract = useCallback(async () => {
+    try {
+      const tokenURI = await contract721.tokenURI(id);
+      const result = await fetch(tokenURI).then(res => res.json());
+      if (result) {
+        setData({ detail: { metadata: result } });
+      }
+    } catch (error) {}
+    try {
+      const tokenURI = await contract1155.uri(id);
+      const result = await fetch(tokenURI).then(res => res.json());
+      if (result) {
+        setData({ detail: { metadata: result } });
+      }
+    } catch (error) {}
+  }, [id, contract721, contract1155]);
+
   useEffect(() => {
     setLoading(true);
 
@@ -186,6 +229,7 @@ export function NFTDetail(props) {
       })
       .catch(e => {
         setData(e.response?.data || {});
+        reqNFTContract();
       })
       .finally(() => {
         setLoading(false);
@@ -197,7 +241,7 @@ export function NFTDetail(props) {
         symbol,
       });
     });
-  }, [address, id]);
+  }, [address, id, reqNFTContract]);
 
   const handleRefresh = useCallback(
     e => {
@@ -266,6 +310,7 @@ export function NFTDetail(props) {
               tokenId={id}
               type="primary"
               enable3D={true}
+              metadata={data.detail?.metadata}
             />
           </Card>
 
