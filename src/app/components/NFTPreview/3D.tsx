40b34ca14f90655d1c24d28e325ce3771e605bb6
mosi <mosshengqi@outlook.com>
2024-01-31 10:19:40 +0800

    fix: 3d render

--- src/app/components/NFTPreview/3D.tsx
+++ src/app/components/NFTPreview/3D.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import '@google/model-viewer';
+import type { ModelViewerElement } from '@google/model-viewer';
 
-export const ThreeD = ({ url = '' }) => {
+export const ThreeD = ({
+  url = '',
+  onError,
+}: {
+  url?: string;
+  onError?: VoidFunction;
+}) => {
+  const ref = useRef<ModelViewerElement>(null);
+  useEffect(() => {
+    const dom = ref.current;
+    if (dom && onError) {
+      dom.addEventListener?.('error', onError);
+    }
+    return () => {
+      if (dom && onError) {
+        dom.removeEventListener?.('error', onError);
+      }
+    };
+  }, [onError]);
   return (
     <Container>
       {/* @ts-ignore */}
       <model-viewer
+        ref={ref}
         alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
         src={url}
         // poster="https://vbs-staging.oss-cn-beijing.aliyuncs.com/pattern/thumbnail1024/prop_mask_004_ue_1024.png?versionid=CAEQKRiBgMCc0Jf.oxgiIDU2MGY0Nzc3ZjBjYjRkYmQ4MjdhNzFlMDk5ZWQ3ZjQ1"
