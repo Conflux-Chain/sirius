cf23fa39ee835ab603f9aed7e94e3319768c9022
cryptosunshine <shouhui.sun@gmail.com>
2024-03-19 18:55:29 +0800

    fix: merge

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,29 +1,9 @@
 import SDK from 'js-conflux-sdk/dist/js-conflux-sdk.umd.min.js';
 import lodash from 'lodash';
+import { LOCALSTORAGE_KEYS_MAP } from 'sirius-next/packages/common/dist/utils/constants';
 
-/**
- * @todo
- * 1. setNFTCacheInfo cacheKey
- * 2. GlobalTip
- *
- * @export
- * @enum {number}
- */
-export enum LOCALSTORAGE_KEYS_MAP {
-  reqProjectConfigMD5 = 'CONFLUX_SCAN_REQ_PROJECT_CONFIG_MD5',
-  networkId = 'CONFLUX_SCAN_NETWORK_ID',
-  contracts = 'CONFLUX_SCAN_CONTRACTS',
-  contractNameTag = 'CONFLUX_SCAN_CONTRACT_NAME_TAG',
-  currency = 'CONFLUX_SCAN_LOCALSTORAGE_KEY_CURRENCY',
-  ageFormat = 'CONFLUX_SCAN_TABLE_AGE_FORMAT',
-  cookieAgreed = 'CONFLUXSCAN_COOKIE_AGREED',
-  txnRecords = 'CONFLUXSCAN_TXN_RECORDS',
-  fccfxNotice = 'CONFLUX_SCAN_FCCFX_NOTICE',
-  addressLabel = 'CONFLUX_SCAN_ADDRESS_LABELS',
-  txPrivateNote = 'CONFLUX_SCAN_TX_PRIVATE_NOTES',
-  hideInDotNet = 'CONFLUX_SCAN_HIDE_IN_DOT_NET',
-  apis = 'CONFLUX_SCAN_APIS',
-}
+export { LOCALSTORAGE_KEYS_MAP }
+
 
 interface ContractsType {
   faucet: string;
