b418be3d3f30baf7e059ce54d5778c504c977f9b
cryptosunshine <shouhui.sun@gmail.com>
2024-03-25 09:51:12 +0800

    fix: move hooks

--- src/utils/hooks/useAge.ts
+++ src/utils/hooks/useAge.ts
@@ -1,17 +1,6 @@
-import { useEffect, useState } from 'react';
-import { LOCALSTORAGE_KEYS_MAP } from '../constants';
+import React from 'react';
+import { useAge as useAgeOriginal } from 'sirius-next/packages/common/dist/utils/hooks/useAge';
 
 export const useAge = (format?: string) => {
-  const [ageFormat, toggleAgeFormat] = useState(
-    format ||
-      localStorage.getItem(LOCALSTORAGE_KEYS_MAP.ageFormat) !== 'datetime'
-      ? 'age'
-      : 'datetime',
-  );
-  useEffect(() => {
-    if (localStorage.getItem(LOCALSTORAGE_KEYS_MAP.ageFormat) !== ageFormat) {
-      localStorage.setItem(LOCALSTORAGE_KEYS_MAP.ageFormat, ageFormat);
-    }
-  }, [format, ageFormat]);
-  return [ageFormat, toggleAgeFormat];
+  return useAgeOriginal(React, format);
 };
