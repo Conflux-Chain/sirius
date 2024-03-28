b87c65bfd3de256eab1d68f32b0d3aec9fa7e954
cryptosunshine <shouhui.sun@gmail.com>
2024-03-28 18:44:15 +0800

    feat: integrated chart

--- src/app/containers/Charts/pow/TPS.tsx
+++ src/app/containers/Charts/pow/TPS.tsx
@@ -3,11 +3,13 @@ import dayjs from 'dayjs';
 import { useTranslation } from 'react-i18next';
 import { translations } from 'locales/i18n';
 import {
-  StockChartTemplate,
+  // StockChartTemplate,
   ChildProps,
 } from 'app/components/Charts/StockChartTemplate';
+import { StockChartTemplate } from 'sirius-next/packages/common/dist/components/Charts/StockChartTemplate';
+import { PreviewChartTemplate } from 'sirius-next/packages/common/dist/components/Charts/PreviewChartTemplate';
+import { scope } from 'sirius-next/packages/common/dist/components/Charts/config';
 import { OPEN_API_URLS } from 'utils/constants';
-import { Wrapper } from './Wrapper';
 import {
   xAxisCustomLabelHour,
   tooltipCustomLabel,
@@ -23,6 +25,12 @@ export function TPS({ preview = false }: ChildProps) {
     subtitle: t(translations.highcharts.pow.tps.subtitle),
     request: {
       url: OPEN_API_URLS.tps,
+      query: preview
+        ? {
+            limit: '30',
+            intervalType: 'day',
+          }
+        : undefined,
       formatter: data => {
         return [
           data?.list?.map(s => [
@@ -38,8 +46,29 @@ export function TPS({ preview = false }: ChildProps) {
       chart: {
         zoomType: 'x',
       },
+      header: {
+        title: {
+          text: t(translations.highcharts.pow.tps.title),
+        },
+        subtitle: {
+          text: t(translations.highcharts.pow.tps.subtitle),
+        },
+        breadcrumb: [
+          {
+            name: t(translations.highcharts.pow.breadcrumb.charts),
+            path: '/pow-charts',
+          },
+          {
+            name: t(translations.highcharts.pow.breadcrumb.tps),
+            path: '/pow-charts/tps',
+          },
+        ],
+      },
       title: {
         text: t(translations.highcharts.pow.tps.title),
+      },
+      subtitle: {
+        text: t(translations.highcharts.subtitle),
       },
       xAxis: {
         type: 'datetime',
@@ -65,12 +94,17 @@ export function TPS({ preview = false }: ChildProps) {
           ...xAxisCustomLabelHour,
         },
       },
+      intervalScope: {
+        min: scope.min,
+        hour: scope.hour,
+        day: scope.day,
+      },
     },
   };
 
-  return (
-    <Wrapper {...props}>
-      <StockChartTemplate {...props}></StockChartTemplate>
-    </Wrapper>
+  return preview ? (
+    <PreviewChartTemplate {...props}></PreviewChartTemplate>
+  ) : (
+    <StockChartTemplate {...props}></StockChartTemplate>
   );
 }
