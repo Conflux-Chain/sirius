09a3e3884f2d063154d34a8da72e50604887b2a0
mosi <mosshengqi@outlook.com>
2024-04-22 16:37:00 +0800

    feat: move Empty

--- src/app/components/TablePanelNew/index.tsx
+++ src/app/components/TablePanelNew/index.tsx
@@ -13,7 +13,7 @@ import { toThousands, formatNumber } from 'utils';
 import { useBreakpoint } from 'styles/media';
 import styled from 'styled-components';
 import clsx from 'clsx';
-import { Empty } from 'app/components/Empty/Loadable';
+import { Empty } from 'sirius-next/packages/common/dist/components/Empty';
 
 interface TableProp extends Omit<TableProps<any>, 'title' | 'footer'> {
   url?: string;
