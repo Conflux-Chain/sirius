0891ce1fc372ed2f56ab96abe83dd97ba7a84064
mosi <mosshengqi@outlook.com>
2024-04-28 14:50:05 +0800

    feat: move TextLogo

--- src/app/containers/Footer/index.tsx
+++ src/app/containers/Footer/index.tsx
@@ -10,7 +10,7 @@ import styled from 'styled-components';
 import { Link } from 'sirius-next/packages/common/dist/components/Link';
 import { media } from 'sirius-next/packages/common/dist/utils/media';
 import { Footer as FooterComp } from 'sirius-next/packages/common/dist/components/Footer';
-import { TextLogo } from 'app/components/TextLogo/Loadable';
+import { TextLogo } from 'sirius-next/packages/common/dist/components/TextLogo';
 import { translations } from 'locales/i18n';
 import { Language } from './Language';
 // import { Currency } from './Currency';
