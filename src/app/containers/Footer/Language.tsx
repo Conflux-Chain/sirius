8dec30791ec62d6193ecbc192de863c280fdb840
cryptosunshine <shouhui.sun@gmail.com>
2024-06-03 14:21:23 +0800

    fix: modify footer color style

--- src/app/containers/Footer/Language.tsx
+++ src/app/containers/Footer/Language.tsx
@@ -65,25 +65,7 @@ const StyledWrapper = styled.span`
   align-items: center;
   color: var(--theme-color-gray0);
 
-  &:hover {
-    color: var(--theme-color-blue0);
-
-    .select.sirius-select.btnSelectContainer .value .option {
-      color: var(--theme-color-blue0) !important;
-    }
-  }
-
-  .select.sirius-select.btnSelectContainer {
-    background: transparent;
-    position: absolute;
-    top: -0.6071rem;
-    left: 0.2143rem;
-
-    .value .option {
-      font-weight: normal;
-      &:not(:hover) {
-        color: var(--theme-color-gray0);
-      }
-    }
+  .btnSelectContainer {
+    background-color: var(--theme-color-foot-bg);
   }
 `;
