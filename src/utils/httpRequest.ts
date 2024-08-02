d4b9e35bac198ed988ec07145c077d4e79341a9b
mosi <mosshengqi@outlook.com>
2024-08-02 16:59:51 +0800

    feat: update version

--- src/utils/httpRequest.ts
+++ src/utils/httpRequest.ts
@@ -79,7 +79,7 @@ export const reqContract = fetchWithCache(
   },
   {
     key: 'contract',
-    maxAge: 1000 * 60 * 10,
+    maxAge: 1000 * 60 * 60,
   },
 );
 
@@ -93,7 +93,7 @@ export const reqContractAndToken = fetchWithCache(
   },
   {
     key: 'contract-and-token',
-    maxAge: 1000 * 60 * 10,
+    maxAge: 1000 * 60 * 60,
   },
 );
 
