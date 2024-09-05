15d6a29fa4b17ad500b18450ac1c2aee9c8a81b7
mosi <mosshengqi@outlook.com>
2024-09-05 15:56:40 +0800

    fix: table sort

--- src/app/components/TablePanelNew/index.tsx
+++ src/app/components/TablePanelNew/index.tsx
@@ -12,6 +12,7 @@ import { useBreakpoint } from '@cfxjs/sirius-next-common/dist/utils/media';
 import styled from 'styled-components';
 import clsx from 'clsx';
 import { Empty } from '@cfxjs/sirius-next-common/dist/components/Empty';
+import { invert } from 'lodash';
 
 type SortOrder = 'descend' | 'ascend' | null;
 type SortDirections = SortOrder[];
@@ -146,10 +147,12 @@ export const TablePanel = ({
     loading: false,
     error: null,
   });
-  const { query: outerQuery, url: queryUrl } = useMemo(
-    () => qs.parseUrl(outerUrl || ''),
-    [outerUrl],
-  );
+  const { query: outerQuery, url: queryUrl } = useMemo(() => {
+    const parsed = qs.parseUrl(outerUrl || '');
+    const q = parsed.query;
+    q.orderBy = invert(sortKeyMap)[(q.orderBy as string) ?? ''] || q.orderBy;
+    return parsed;
+  }, [sortKeyMap, outerUrl]);
   const { orderBy, reverse } = useMemo(
     () => ({ ...outerQuery, ...qs.parse(search) }),
     [search, outerQuery],
@@ -182,6 +185,8 @@ export const TablePanel = ({
   useEffect(() => {
     if (queryUrl) {
       const query = { ...getQuery } as qs.ParsedQuery<string>;
+      query.orderBy =
+        sortKeyMap[(query.orderBy as string) ?? ''] || query.orderBy;
       if (sortParam === 'sort') {
         if (query.reverse === 'false') {
           query.sort = 'asc';
@@ -231,7 +236,7 @@ export const TablePanel = ({
     };
 
     if (sorter?.order) {
-      query.orderBy = sortKeyMap[String(sorter.field)] || sorter.field;
+      query.orderBy = sorter.field;
       query.reverse = sorter.order === 'ascend' ? 'false' : 'true';
     } else {
       delete query.orderBy;
