b62d71d111b177ce748b1c402e87b8b80b2f5e75
mosi <mosshengqi@outlook.com>
2025-05-19 14:08:51 +0800

    optimize

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@
 
 // import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from 'serviceWorker';
